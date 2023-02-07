643e8055ee4637cb9864adbb1287e96f4dab867c
Matstaps <matstaplehurst@yahoo.co.uk>
2023-02-07 16:25:33 +0000

    Amend code to JS and HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,3 @@
-// functions
-
 const choices = {
 
     0: "Fire",
@@ -7,21 +5,19 @@ const choices = {
     2: "Wood",
 }
 
-//var for choices
 var userScore = 0;
 var comScore = 0;
 var userScoreEl;
 var comScoreEl;
 var winnerEl;
 
-//var for user score
+
 
 function userChoice(event) {
     console.log('You chose:', event.target.innerText);
     const userChoiceInt = event.target.dataset.choice;
     return userChoiceInt;
 }
-//var for com score
 
 function comChoice(){
 
@@ -34,59 +30,48 @@ function score(event){
 
     var userChoiceInt = userChoice(event);
     var comChoiceInt = comChoice();
-    console.log("User: " + userChoiceInt + "Computer: " + comChoiceInt);
+    console.log("User: " + userChoiceInt + " Computer: " + comChoiceInt);
 
-    if (userChoiceInt === comChoiceInt){
+    if (userChoiceInt == comChoiceInt){
             console.log('Its a Draw!');
-            winnerEl.innerHTMl = "It's a Draw!";
-    }   else if (userChoiceInt == 0 && comChoiceInt == 1){
+            winnerEl.innerHTML = "It's a Draw!";
+        }   else if (userChoiceInt == 0 && comChoiceInt == 1){
             console.log('Computer Wins!');
             comScore ++;
-            winnerEl.innerHTMl = "Computer Wins!";
-    }   else if (userChoiceInt == 0 && comChoiceInt == 2){
+            winnerEl.innerHTML = "Computer Wins!";
+        }   else if (userChoiceInt == 0 && comChoiceInt == 2){
             console.log('Player Wins!');
             userScore ++;
-            winnerEl.innerHTMl = "Player Wins!";
-    }   else if (userChoiceInt == 1 && comChoiceInt == 0){
+            winnerEl.innerHTML = "Player Wins!";
+        }   else if (userChoiceInt == 1 && comChoiceInt == 0){
             console.log('Player Wins!');
             userScore ++;
-            winnerEl.innerHTMl = "Player Wins!";
-    }   else if (userChoiceInt == 1 && comChoiceInt == 2){
+            winnerEl.innerHTML = "Player Wins!";
+        }   else if (userChoiceInt == 1 && comChoiceInt == 2){
             console.log('Computer Wins!');
             comScore ++;
-            winnerEl.innerHTMl = "Computer Wins!";
-    }   else if (userChoiceInt == 2 && comChoiceInt == 0){
+            winnerEl.innerHTML = "Computer Wins!";
+        }   else if (userChoiceInt == 2 && comChoiceInt == 0){
             console.log('Computer Wins!');
             comScore ++;
-            winnerEl.innerHTMl = "Computer Wins!";
-    }   else if (userChoiceInt == 2 && comChoiceInt == 1){
+            winnerEl.innerHTML = "Computer Wins!";
+        }   else if (userChoiceInt == 2 && comChoiceInt == 1){
             console.log('Player Wins!');
             userScore ++;
-            winnerEl.innerHTMl = "Player Wins!";
-    }
+            winnerEl.innerHTML = "Player Wins!";
+        }
 
-    userScoreEl.innerHTMl = "User: " + userScore;
-    comScoreEl.innerHTMl = "Computer: " + comScore;
+    userScoreEl.innerHTML = "User: " + userScore;
+    comScoreEl.innerHTML = "Computer: " + comScore;
 
 }
-
-//var for user accum score
-//var for com accum score
-
-// function for user choice
-
-// function for computer choice
-
-// function for score
-
-// function dom load, buttons
 
 window.addEventListener('DOMContentLoaded', (event) => {
     console.log('DOM fully loaded and parsed');
 
     const btns = document.querySelectorAll('button');
 
-    console.log('I found ' + btns.length + 'buttons');
+    console.log('I found ' + btns.length + ' buttons');
 
     btns.forEach(btn => {
         
