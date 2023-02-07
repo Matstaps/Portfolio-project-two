f16b14b2d095488b6ded6b0ed6d7823c097f2138
Matstaps <matstaplehurst@yahoo.co.uk>
2023-02-07 12:34:41 +0000

    Add player and com choice functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,31 @@
 // functions
 
+const choices = {
+
+    0: "Fire",
+    1: "Water",
+    2: "Wood",
+}
+
 //var for choices
 
 //var for user score
+
+function userChoice(event) {
+    console.log('You chose:', event.target.innerText);
+    const userChoiceInt = event.target.dataset.choice;
+    return userChoiceInt;
+}
 //var for com score
+
+function comChoice(){
+
+    const conRandom = Math.floor(Math.random() * 3);
+    console.log('Computer chose:', choices[comRandom]);
+    return.comRandom;
+}
+
+
 
 //var for user accum score
 //var for com accum score
