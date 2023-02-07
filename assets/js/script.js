835d9f1bcc909167793e0f0eaf55adf58ff20ffd
Matstaps <matstaplehurst@yahoo.co.uk>
2023-02-07 12:12:58 +0000

    Revise js functions and vars

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,107 +1,27 @@
+// functions
 
-const btn = document.querySelector('button');
+//var for choices
 
-function playerChoice(event) {
-    console.log('you picked your element', event.target.id);
-}
+//var for user score
+//var for com score
 
-btn.addEventListener('click', playerChoice);
+//var for user accum score
+//var for com accum score
 
-function randomCom(0, 2) {
-    return Math.floor(Math.random() * (0 - 2 + 1)) + min;
-}
-// *document.addEventListener('DOMContentLoaded'); {
-//   console.log('Ready to play');
-//    alert('ready to play');
-//    let buttons = document.getElementsByTagName("button");
-// }
+// function for user choice
 
+// function for computer choice
 
-    
+// function for score
 
+// function dom load, buttons
 
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
 
-// create array water wood fire
-// create run game function
-// create event listener for button click
-// create random selection from array for computer after clicj
-// create result calculator
-// create score accumulator
+    const btns = document.querySelectorAll('button');
 
-//function to create variables
-//constant playRound() {
-    //create variables
-//   const woodBtn = document.querySelector.apply('.wood');
-//    const fireBtn = document.querySelector.apply('.fire');
-//    const waterBtn = document.querySelector.apply('.water');
-//    const playerChoice = [woodBtn, fireBtn, waterBtn];
-//    const comChoice = ['wood', 'water', 'fire']
-//}
-
-//function to start playing
-
-//playerChoice(choice) {
-//    choice.addEventListener('click', function) {
-
-//        const comChoiceNum = Math.floor(Math.random()*3);
-//        const comPick = comChoice[comChoiceNum];
-//          need to innerHTML to player-box and computer-box
-//    }
-//    }
-
-//result
-
-// const win = (player,computer) {
-//    const = result document.querySelector('.result');
-
-//    if (player === computer) {
-//        result.text = 'Draw'
-
-//    } else if (player == 'wood'){
-//        if (computer == 'water'){
-//            result.textContext = 'Player wins'
-//            Player score ++
-//      } else {
-//            result.textContext = 'Computer wins'
-//            Computer score ++
-//        }
-//    } else if (player == 'water'){
-//        if (computer == 'wood'){
-//            result.textContext = 'Computer wins'
-//            Computer score ++
-//        } else {
-//            result.textContext = 'Player wins'
-//            Player score ++
-//        }
-//    }   else if (player == 'fire'){
-//        if (computer == 'water'){
-//            result.textContext = 'Computer wins'
-//            Computer score ++
-//        } else {
-//            result.textContext = 'Player wins'
-//            Player score ++
-//        }
-//    }  
-//} 
-
-/**
- * Accumulate player score
- * 
- * function PlayerTally {
- * let oldPScore = parseInt(document.GetElementById('player-score-box').innerText);
- * document.GetElementById('player-score-box').innerText= ++oldPscore;
- * }
- */
-
-/**
- * Accumulate computer score
- * 
- * function ComputerTally {
- * let oldCScore = parseInt(document.GetElementById('computer-score-box').innerText);
- * document.GetElementById('computer-score-box').innerText= ++oldCscore;
- * }
- */
+    console.log('I found ' + btns.length + 'buttons');
 
 
-
-
+});
